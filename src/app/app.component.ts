efcfe054b46230a83c40a1c475bf80dcf8681231
Jheff <divegas43@gmail.com>
2025-03-08 18:04:08 -0500

    Added: basic configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,68 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import {
+  Component,
+  Inject,
+  inject,
+  OnDestroy,
+  PLATFORM_ID,
+} from '@angular/core';
+import { NavigationEnd, Router, RouterOutlet } from '@angular/router';
+import { MatIconRegistry } from '@angular/material/icon';
+import { isPlatformBrowser } from '@angular/common';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'frontend-ipuc';
+export class AppComponent implements OnDestroy {
+  private _iconRegistry: MatIconRegistry = inject(MatIconRegistry);
+  private readonly _router: Router = inject(Router);
+  private _routerSubscription!: Subscription;
+
+  constructor(@Inject(PLATFORM_ID) private platformId: object) {
+    this._setMaterialOutlinedIconsDefault();
+    this._listenRouterChanges();
+  }
+
+  /**
+   * Para definir el uso de iconos tipo outlined en la App de manera global
+   * @private
+   */
+  private _setMaterialOutlinedIconsDefault(): void {
+    const defaultFontSetClasses = this._iconRegistry.getDefaultFontSetClass();
+    const outlinedFontSetClasses = defaultFontSetClasses
+      .filter(
+        (fontSetClass: string): boolean => fontSetClass !== 'material-icons'
+      )
+      .concat(['material-icons-outlined']);
+    this._iconRegistry.setDefaultFontSetClass(...outlinedFontSetClasses);
+  }
+
+  /**
+   * Para escuchar los cambios de rutas
+   * @private
+   */
+  private _listenRouterChanges(): void {
+    this._routerSubscription = this._router.events.subscribe((event): void => {
+      if (event instanceof NavigationEnd) {
+        this._setScrollOnTop();
+      }
+    });
+  }
+
+  /**
+   * Para mover el scroll al inicio de la pagina
+   * @private
+   */
+  private _setScrollOnTop(): void {
+    if (isPlatformBrowser(this.platformId)) {
+      window.scrollTo({ top: 0, behavior: 'smooth' });
+    }
+  }
+
+  ngOnDestroy(): void {
+    this._routerSubscription.unsubscribe();
+  }
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,16 +1,50 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  LOCALE_ID,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
 import {
   MAT_NATIVE_DATE_FORMATS,
   provideNativeDateAdapter,
 } from '@angular/material/core';
 
+import { ToastrModule } from 'ngx-toastr';
 import { routes } from './app.routes';
+import { MatPaginatorIntl } from '@angular/material/paginator';
+import { getMaterialPaginatorTranslations } from './shared/utilities/material-paginator-translations';
+import { MAT_DIALOG_DEFAULT_OPTIONS } from '@angular/material/dialog';
+import { provideHttpClient } from '@angular/common/http';
+import { TransformDateService } from './shared/services/transform-date.service';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { registerLocaleData } from '@angular/common';
+import localeEs from '@angular/common/locales/es';
+
+registerLocaleData(localeEs);
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideNativeDateAdapter(MAT_NATIVE_DATE_FORMATS),
+    provideAnimationsAsync(),
+    importProvidersFrom(
+      ToastrModule.forRoot({
+        preventDuplicates: true,
+      })
+    ),
+    { provide: MatPaginatorIntl, useValue: getMaterialPaginatorTranslations() },
+    {
+      provide: MAT_DIALOG_DEFAULT_OPTIONS,
+      useValue: { maxWidth: '700px', width: '95vw', padding: '40px' },
+    },
+    { provide: LOCALE_ID, useValue: 'es' },
+    provideHttpClient(),
+    /* 
+    TODO: Habilitar interceptores cuando se creen 
+    withInterceptors([authInterceptor, notificationsInterceptor])
+    */
+    TransformDateService,
   ],
 };
