bfa3654b8d47bd3ee637e38c22db51cf540fa231
Jairo-18 <jhonlegarda1.2@gmail.com>
2025-03-14 18:11:25 -0500

    Ajust: hors and ubication in about

--- src/app/public/components/ubication-hours/ubication-hours.component.ts
+++ src/app/public/components/ubication-hours/ubication-hours.component.ts
@@ -1,10 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { MatIcon } from '@angular/material/icon';
 
 @Component({
   selector: 'app-ubication-hours',
-  imports: [MatIcon],
+  imports: [MatIcon, CommonModule],
   templateUrl: './ubication-hours.component.html',
   styleUrl: './ubication-hours.component.scss',
 })
-export class UbicationHoursComponent {}
+export class UbicationHoursComponent {
+  diasSemana: { nombre: string; evento: string }[] = [
+    { nombre: 'Lunes', evento: 'Culto a las 8:00 PM' },
+    { nombre: 'Martes', evento: 'Culto a las 8:00 PM' },
+    { nombre: 'Miércoles', evento: 'Culto a las 8:00 PM' },
+    { nombre: 'Jueves', evento: 'Culto a las 8:00 PM' },
+    { nombre: 'Viernes', evento: 'Culto a las 8:00 PM' },
+    { nombre: 'Sábado', evento: 'Culto a las 8:00 PM' },
+    { nombre: 'Domingo', evento: 'Culto a las 8:00 PM' },
+  ];
+}
