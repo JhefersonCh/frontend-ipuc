ca0facc6775084e167c003c5a2577cb0d997ff82
Jairo-18 <jhonlegarda1.2@gmail.com>
2025-03-15 21:13:58 -0500

    Fixed: mobile responsive

--- src/app/layout/components/nav-bar/nav-bar.component.ts
+++ src/app/layout/components/nav-bar/nav-bar.component.ts
@@ -1,11 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { MatIcon } from '@angular/material/icon';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [RouterLink, CommonModule],
+  imports: [RouterLink, CommonModule, MatIcon],
   templateUrl: './nav-bar.component.html',
   styleUrl: './nav-bar.component.scss',
 })
-export class NavBarComponent {}
+export class NavBarComponent {
+  menuOpen: boolean = false;
+  menuClose() {
+    this.menuOpen = false;
+  }
+}
