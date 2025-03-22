77dac2b87a3a64d01f09d9ecd83b19c1b97b2a8c
Jairo-18 <jhonlegarda1.2@gmail.com>
2025-03-22 09:18:08 -0500

    Added: change password and estiliced in about

--- src/app/user/services/user-data.service.ts
+++ src/app/user/services/user-data.service.ts
@@ -4,6 +4,7 @@ import { User } from '../../shared/interfaces/user.interface';
 import { ApiResponseInterface } from '../../shared/interfaces/api-response.interface';
 import { Observable } from 'rxjs';
 import { environment } from '../../../environments/environment.development';
+import { PasswordInterface } from '../../auth/interfaces/passwords';
 
 @Injectable({
   providedIn: 'root',
@@ -21,6 +22,13 @@ export class UserDataService {
   updateUserProfile(id: string, body: unknown): Observable<void> {
     return this._httpClient.patch<void>(
       `${environment.apiUrl}user/${id}`,
+      body
+    );
+  }
+
+  changePasswordUser(body: unknown): Observable<PasswordInterface> {
+    return this._httpClient.patch<PasswordInterface>(
+      `${environment.apiUrl}user/change-password`, // Nueva ruta
       body
     );
   }
