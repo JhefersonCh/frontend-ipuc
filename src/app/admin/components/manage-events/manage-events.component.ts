050fb714d5badf3d68406a718db405ec4d11467d
Jheff <divegas43@gmail.com>
2025-05-17 19:43:24 -0500

    Add: close dialog to delete event

--- src/app/admin/components/manage-events/manage-events.component.ts
+++ src/app/admin/components/manage-events/manage-events.component.ts
@@ -184,6 +184,7 @@ export class ManageEventsComponent {
     });
     dialogRef.componentInstance.yesEvent.subscribe(() => {
       this._deleteEvent(event);
+      dialogRef.close();
     });
   }
 
